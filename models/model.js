9d915893e8b3914afeb855c92af74e6c629716f8
Chong Qiang Chew <chewchongqiang@gmail.com>
2017-12-01 19:11:23 +0800

    push

---
+++ models/model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const bcrypt = require('bcrypt')
+
+const modelSchema = new Schema({
+  itemCode: String,
+  model: String,
+  color: String,
+  dimension: String,
+
+})
