025c679c51d6d00e16514af15f5ef06951c5987a
thamjieying <thamjieying@gmail.com>
2017-12-03 15:39:37 +0800

    trying to merge

---
+++ routes/customer_routes.js
@@ -0,0 +1,8 @@
+const Customer = require('../models/customer')
+const express = require('express')
+const router = express.Router()
+
+router.get('/new',(req,res) => {
+  res.render('customers/new')
+
+})
