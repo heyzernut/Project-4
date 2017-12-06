990a50a384e499490d04370a072e5f399bcc15c2
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-12-06 16:24:03 +0800

    staff able to CRUD

--- routes/staff_routes.js
+++ routes/staff_routes.js
@@ -33,7 +33,9 @@ router.post('/', (req, res, next) => {
 router.get('/', (req, res) => {
   // the return of then
   Staff.find()
+  .populate('role')
   .then(staffs => {
+    // console.log()
     res.render('staffs/view', {
       staffs
     })
@@ -47,10 +49,13 @@ router.get('/update/:id', (req, res) => {
 
   Staff
     .findById(req.params.id)
+    .populate('role')
     .then(staff => {
       Role.find()
       .then((role) => {
-        res.render('staffs/update', {staff, role})
+        const selected = staff.role
+        const filteredRole = role.filter((e) => e.name !== selected.name)
+        res.render('staffs/update', {staff, role: filteredRole, selected})
       })
     })
     .catch(err => {
@@ -59,16 +64,29 @@ router.get('/update/:id', (req, res) => {
 })
 
 router.put('/update/:id', (req, res) => {
-
+  // console.log('Updating PW!! :, ', req.body.staff)
   var formData = req.body.staff
-  Staff
-  .findByIdAndUpdate(req.params.id, {
-      name: formData.txtName,
-      role: formData.role,
-      password: formData.txtPassword
+  Staff.findById(req.params.id)
+    .populate('role')
+    .then((user) => {
+      console.log('from the form:', formData.role)
+      Role.find({name: formData.role})
+        .then((role) => {
+          console.log('role : ',role)
+          console.log(role[0].name);
+          user.set({
+            role: role[0]._id,
+            name: formData.txtName,
+            password: formData.txtPassword
+          })
+          user.save()
+          .then(
+            (updatedUser) => res.redirect(`/staffs`),
+            (err) => res.send(err)
+          )
+        })
+
     })
-    .then(() => res.redirect(`/roles`))
-    .catch(err => console.log(err))
 
 })
 
