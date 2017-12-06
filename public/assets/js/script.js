0a7466533e7a3c2219762bf99bd67e4f016e0470
thamjieying <thamjieying@gmail.com>
2017-12-06 18:51:36 +0800

    deliveryorder update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,25 +1,39 @@
-
 $(document).ready(function(){
   $('#example').DataTable();
+
+
+  //orderform function
+  $('.orderType').on('change',function(){
+    if( $(this).val()==="demo"){
+      $(".returnDate").prop('disabled', false)
+      // $("#row-1-returnDate").show()
+    }
+    else{
+      $( ".returnDate" ).prop('disabled', true);
+      // $("#row-1-returnDate").hide()
+
+    }
+  });
+  //add rows
+  $( "#addRow" ).click(function() {
+    const markup = '<tr><td><input type="text" id="row-1-itemName" name="row-1-itemName" placeholder="Item Name"></td><td><select size="1" id="row-1-orderType" class="orderType" name="row-1-orderType"><option value="sales" selected="selected">Sales</option><option value="demo">Demo</option></select></td><td><input type="date" id="row-1-returnDate" class="returnDate" name="row-1-returnDate" value="{{today}}" disabled></td><td><input type="number" id="row-1-orderQuantity" name="row-1-orderQuantity" value="1"></td><td></td></tr>';
+    $("#orderItems>tbody").append(markup);
+  });
+
+  //location form
+  $('#selectLocation').on('change',function(){
+    if( $(this).val()==="Warehouse"){
+      $("#otherType").show()
+    }
+    else{
+      $("#otherType").hide()
+    }
+  });
+
 });
 
-$('#selectLocation').on('change',function(){
-     if( $(this).val()==="Warehouse"){
-     $("#otherType").show()
-     }
-     else{
-     $("#otherType").hide()
-     }
- });
 
- $('#orderType').on('change',function(){
-      if( $(this).val()==="demo"){
-      $("#returnDate").show()
-      }
-      else{
-      $("#returnDate").hide()
-      }
-  });
+
 
   // var FormStuff = {
   //
